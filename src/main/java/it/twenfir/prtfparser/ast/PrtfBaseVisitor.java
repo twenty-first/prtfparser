79e7d6cdd2da0d0b5f4389ec7ec04b89783a685f
Nicola Musatti <nicola.musatti@gmail.com>
2024-01-22 10:31:28 +0100

    Add AST visitor stub

---
+++ src/main/java/it/twenfir/prtfparser/ast/PrtfBaseVisitor.java
@@ -0,0 +1,12 @@
+package it.twenfir.prtfparser.ast;
+
+import it.twenfir.antlr.ast.BaseAstVisitor;
+
+public class PrtfBaseVisitor<ValueT> extends BaseAstVisitor<ValueT> implements PrtfVisitor<ValueT> {
+
+	@Override
+	public ValueT visitPrtf(Prtf node) {
+        return visitChildren(node);
+	}
+
+}
---
+++ src/main/java/it/twenfir/prtfparser/ast/PrtfVisitor.java
@@ -0,0 +1,7 @@
+package it.twenfir.prtfparser.ast;
+
+import it.twenfir.antlr.ast.AstVisitor;
+
+public interface PrtfVisitor<ValueT> extends AstVisitor<ValueT> {
+	ValueT visitPrtf(Prtf Node);
+}
